2a109ac4365334eee29abf0e9d77cbc3b1a577fb
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-03-03 17:34:27 -0500

    fix: typo

--- nixos-install.sh
+++ nixos-install.sh
@@ -2,7 +2,7 @@
 
 # Config constants
 # readonly DISK
-# DISK="/dev/vda1"
+# DISK="/dev/vda"
 
 readonly MEMORY
 MEMORY="8G"
@@ -18,7 +18,7 @@ nix run github:nix-community/disko \
 	--extra-experimental-features flakes \
 	-- \
 	--mode zap_create_mount /tmp/disko-config.nix \
-	--arg disks '[ "/dev/vda1" ]' ||
+	--arg disks '[ "/dev/vda" ]' ||
 	exit 1
 # \ --arg memory 'MEMORY'
 
